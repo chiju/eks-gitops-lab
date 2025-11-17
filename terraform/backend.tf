1caa39216cff56b7c114565e7c34d96f0c987a42
chiju <chijumelveettil@gmail.com>
2025-11-17 21:33:54 +0100

    chore: Update S3 backend bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 # For GitHub Actions with OIDC - no profile needed
 terraform {
   backend "s3" {
-    bucket       = "eks-gitops-tfstate-d6c97808"
+    bucket       = "eks-gitops-tfstate-bcda8a19"
     key          = "eks-gitops-lab.tfstate"
     region       = "eu-central-1"
     encrypt      = true
